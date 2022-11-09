e9773f6783fcf2cea01c04f9bca760f67274833a
Sazzadu42644 <20-42644-1@student.aiub.edu>
2022-11-10 00:19:27 +0600

    project description

---
+++ resources/views/admin/routeList.blade.php
@@ -0,0 +1,64 @@
+
+@extends('layouts.userView')
+@section('userContent')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>owner DashBoard</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">FCS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('admin')}}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('ListOfRoute')}}">Route List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('ListOfBus')}}">Bus List</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="mt-5">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">Route ID</th>
+            <th scope="col">Route No</th>
+            <th scope="col">Route Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($routeListArr as $route)
+            <tr>
+                <td>{{$route->route_id}}</td>
+                <td>{{$route->route_no}}</td>
+                <td>{{$route->route_name}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+     </div>
+    </div>
+    
+</body>
+
+</html>
+@endsection
